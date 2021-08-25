00142ac17c74522b54aaedb4e64a22cda0bd80a0
Justin Doornbos <doornbosjustin@gmail.com>
2021-08-24 21:35:47 -0400

    fix: remove axes from showing on plots

--- clai/visualization.py
+++ clai/visualization.py
@@ -6,7 +6,6 @@ from .automata import CellularAutomaton1D
 def plot(automaton: CellularAutomaton1D, offset: int = 0, colormap: str = 'Greys', save: bool = False):
     cmap = plt.get_cmap(colormap)
     fig = plt.figure(frameon=False)
-    plt.box(False)
     ax = fig.add_axes([0, 0, 1, 1])
     ax.axis('off')
     plt.imshow(automaton.generations[offset:], interpolation='none', cmap=cmap)
