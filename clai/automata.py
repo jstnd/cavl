b339b82cf2d8250b95739a0c9f4058c8f6af44d3
Justin Doornbos <doornbosjustin@gmail.com>
2021-08-28 00:19:13 -0400

    add built-in function for creating initial cellular automata grids

--- clai/automata.py
+++ clai/automata.py
@@ -1,32 +1,41 @@
+from typing import Any, Union
+
 import numpy as np
+import numpy.typing as npt
 
 from .rules import Base1DRule
 
 
+def init(shape: Union[int, tuple[int, int]], setting: str = "simple", dtype: npt.DTypeLike = np.int, k: int = 2) -> npt.NDArray[npt.DTypeLike]:
+    if setting == "simple":
+        a = np.zeros(shape, dtype=dtype)
+
+        if isinstance(shape, int):
+            a[len(a) // 2] = k - 1
+        else:
+            a[a.shape[0] // 2][a.shape[1] // 2] = k - 1
+
+        return a
+    elif setting == "random":
+        return np.random.randint(k, size=shape, dtype=dtype)
+    else:
+        raise ValueError(f"'{setting}' is not a valid init preset name; supported names are: 'simple', 'random'")
+
+
 class CellularAutomaton1D:
-    def __init__(self, rule: Base1DRule, init: list[int] = None, width: int = 100):
+    def __init__(self, rule: Base1DRule, init: Union[list[Any], npt.NDArray[Any]], width: int = 100):
+        if len(init) != width:
+            raise ValueError("init length must be same as width value (default width is 100)")
+
         self._rule = rule
         self._width = width
-        self.generations = []
+        self.generations = [init]
 
-        if init is None:
-            a = np.zeros(width, dtype=int)
-            a[width // 2] = 1
-            self.generations.append(a)
-        else:
-            if len(init) < width:
-                fill = width - len(init)
-                self.generations.append(np.pad(init, (fill // 2, fill // 2 + (1 if fill % 2 == 1 else 0))))
-            elif len(init) > width:
-                raise ValueError("length of given initial state bigger than given width (default width is 100)")
-            else:
-                self.generations.append(init)
-
-    def next(self) -> list[int]:
+    def next(self) -> list[Any]:
         self.generations.append(self._rule.apply(self.generations[-1]))
         return self.generations[-1]
 
-    def run(self, generations: int) -> list[int]:
+    def run(self, generations: int) -> list[Any]:
         for _ in range(generations):
             self.next()
 
