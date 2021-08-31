d8d4a103a9a9045c0577dd6e350ff5ec99b23352
Justin Doornbos <doornbosjustin@gmail.com>
2021-08-31 19:30:37 -0400

    add ability to adjust dpi of saved plot image

--- clai/visualization.py
+++ clai/visualization.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 from .automata import CellularAutomaton1D
 
 
-def plot(automaton: CellularAutomaton1D, offset: int = 0, colormap: str = 'Greys', save: bool = False):
+def plot(automaton: CellularAutomaton1D, offset: int = 0, colormap: str = 'Greys', save: bool = False, dpi: int = 100):
     cmap = plt.get_cmap(colormap)
     fig = plt.figure(frameon=False)
     ax = fig.add_axes([0, 0, 1, 1])
     ax.axis('off')
     plt.imshow(automaton.generations[offset:], interpolation='none', cmap=cmap)
     if save:
-        plt.savefig('automaton')
+        plt.savefig('automaton', dpi=dpi)
     plt.show()
