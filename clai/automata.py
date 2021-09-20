baf24b5e296699a6857431f992d30a081460d513
Justin Doornbos <doornbosjustin@gmail.com>
2021-09-19 23:14:02 -0400

    modify CellularAutomaton1D class to take list of coordinates as neighbors

--- clai/automata.py
+++ clai/automata.py
@@ -7,7 +7,8 @@ import numpy.typing as npt
 
 def init(shape: Union[int, tuple[int, int]],
          setting: str = "simple",
-         dtype: Type[float] = int, k: int = 2) -> npt.NDArray[float]:
+         dtype: Type[float] = int,
+         k: int = 2) -> npt.NDArray[float]:
     if setting == "simple":
         a = np.zeros(shape, dtype=dtype)
 
@@ -26,8 +27,8 @@ def init(shape: Union[int, tuple[int, int]],
 class CellularAutomaton1D:
     def __init__(self,
                  init: Union[list[float], npt.NDArray[float]],
-                 neighbors: list[int],
-                 apply: Callable[[list[Union[float, int]]], float]):
+                 neighbors: list[tuple[int, int]],
+                 apply: Callable[[dict[tuple[int, int]]], float]):
         self.generations = [init]
         self.neighbors = neighbors
         self.apply = apply
@@ -36,13 +37,14 @@ class CellularAutomaton1D:
     def evolve(self, generations: int = 1) -> None:
         for _ in range(generations):
             next_generation = []
-            for i in range(self.width):
-                neighbors = []
-                left_radius = (len(self.neighbors) - 1) // 2
-                right_radius = math.ceil((len(self.neighbors) - 1) / 2)
-                for step, j in enumerate(range(-left_radius, right_radius + 1)):
-                    neighbors.append(self.generations[-1][(i + j) % self.width] if self.neighbors[step] > 0 else -1)
+            for x in range(self.width):
+                neighbors = {}
+                for neighbor in self.neighbors:
+                    dx, dy = neighbor
+                    neighbors[neighbor] = self.generations[(-1 + dy) % len(self.generations)][(x + dx) % self.width]
+
                 next_generation.append(self.apply(neighbors))
+
             self.generations.append(next_generation)
 
 
