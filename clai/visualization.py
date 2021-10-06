066fabf5c86606e617f5636c239163cf5c705f51
Justin Doornbos <doornbosjustin@gmail.com>
2021-10-06 00:52:13 -0400

    add ability to specify filename for plots/animations

--- clai/visualization.py
+++ clai/visualization.py
@@ -6,19 +6,19 @@ import numpy as np
 from .automata import CellularAutomaton1D, CellularAutomaton2D
 
 
-def plot(automaton: CellularAutomaton1D, offset: int = 0, colormap: str = 'Greys', save: bool = False, dpi: int = 100):
+def plot(automaton: CellularAutomaton1D, offset: int = 0, colormap: str = 'Greys', save: bool = False, filename: str = 'automaton', dpi: int = 100):
     cmap = plt.get_cmap(colormap)
     fig = plt.figure(frameon=False)
     ax = fig.add_axes([0, 0, 1, 1])
     ax.axis('off')
     plt.imshow(automaton.generations[offset:], interpolation='none', cmap=cmap)
     if save:
-        plt.savefig('automaton', dpi=dpi)
+        plt.savefig(filename, dpi=dpi)
     else:
         plt.show()
 
 
-def animate(automaton: CellularAutomaton1D, colormap: str = 'Greys', interval: int = 50, save: bool = False, dpi: int = 100):
+def animate(automaton: CellularAutomaton1D, colormap: str = 'Greys', interval: int = 50, save: bool = False, filename: str = 'automaton.gif', dpi: int = 100):
     # TODO: this is very basic and has issues (expand/optimize)
     
     fig, ax = plt.subplots(frameon=False)
@@ -33,23 +33,23 @@ def animate(automaton: CellularAutomaton1D, colormap: str = 'Greys', interval: i
 
     anim = animation.FuncAnimation(fig, _animate, frames=len(automaton.generations), interval=interval, blit=True)
     if save:
-        anim.save('automaton.gif', writer='ffmpeg', dpi=dpi)
+        anim.save(filename, writer='ffmpeg', dpi=dpi)
     else:
         plt.show()
 
 
-def plot2d(automaton: CellularAutomaton2D, colormap: str = 'Greys', save: bool = False, dpi: int = 100):
+def plot2d(automaton: CellularAutomaton2D, colormap: str = 'Greys', save: bool = False, filename: str = 'automaton', dpi: int = 100):
     fig = plt.figure(frameon=False)
     ax = fig.add_axes([0, 0, 1, 1])
     ax.axis('off')
     plt.imshow(automaton.generations[-1], interpolation='none', cmap=plt.get_cmap(colormap))
     if save:
-        plt.savefig('automaton', dpi=dpi)
+        plt.savefig(filename, dpi=dpi)
     else:
         plt.show()
 
 
-def animate2d(automaton: CellularAutomaton2D, colormap: str = 'Greys', interval: int = 50, save: bool = False, dpi: int = 100):
+def animate2d(automaton: CellularAutomaton2D, colormap: str = 'Greys', interval: int = 50, save: bool = False, filename: str = 'automaton.gif', dpi: int = 100):
     fig, ax = plt.subplots(frameon=False)
     ax.axis('off')
     colormap = plt.get_cmap(colormap)
@@ -60,6 +60,6 @@ def animate2d(automaton: CellularAutomaton2D, colormap: str = 'Greys', interval:
 
     anim = animation.FuncAnimation(fig, _animate, frames=len(automaton.generations), interval=interval, blit=True)
     if save:
-        anim.save('automaton.gif', writer='ffmpeg', dpi=dpi)
+        anim.save(filename, writer='ffmpeg', dpi=dpi)
     else:
         plt.show()
