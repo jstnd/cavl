8a260707bc786772ee8a4ac9e79825375949d92b
Justin Doornbos <doornbosjustin@gmail.com>
2021-10-25 16:51:48 -0400

    change project imports to be more explicit

--- cavl/__init__.py
+++ cavl/__init__.py
@@ -1,6 +1,6 @@
-from .automata import *
-from .neighborhoods import *
-from .rules import *
-from .visualization import *
+from .automata import init, CellularAutomaton1D, CellularAutomaton2D
+from .neighborhoods import moore, von_neumann
+from .rules import General1DRule, Totalistic1DRule
+from .visualization import plot, animate, plot2d, animate2d
 
 __version__ = "0.4.1"
